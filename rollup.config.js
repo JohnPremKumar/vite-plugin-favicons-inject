e57e71a3036f50d87e29c0fdd5017cf91dd6fc8d
John Prem Kumar S <johnpk305@gmail.com>
2022-07-17 18:50:57 +0530

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { eslint } from 'rollup-plugin-eslint';
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
+import commonjs from 'rollup-plugin-commonjs';
+
+const input = [
+  './src/index.js',
+];
+
+export default [
+  {
+    input,
+    plugins: [
+      nodeResolve(),
+      excludeDependenciesFromBundle(),
+      commonjs(),
+      eslint(),
+    ],
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import favicons from 'favicons';
+import fs from 'fs';
+import path from 'path';
+
+const callback = (error, response) => new Promise((resolve, reject) => {
+  if (error) {
+    reject(error);
+  }
+  resolve(response);
+});
+
+const writeFile = (filePath, fileContent) => new Promise((resolve, reject) => {
+  fs.writeFile(filePath, fileContent, (writeFileError) => {
+    if (writeFileError) {
+      reject(writeFileError);
+    }
+    resolve(filePath);
+  });
+});
+
+const viteFaviconsPlugin = (inputSource, inputConfig = {}) => {
+  let response = false;
+  let isProcessed = false;
+  let viteConfig = false;
+  let source = inputSource;
+  let config = inputConfig;
+  return {
+    name: 'vite-plugin-favicon-inject',
+    configResolved(resolvedConfig) {
+      viteConfig = resolvedConfig;
+      // override default source if none set
+      if (!source || source === '') {
+        source = path.join(viteConfig.root, 'src', 'logo.png');
+      }
+      // making sure that config is a object
+      if (!config || (config && config.constructor.name !== 'Object')) {
+        config = {};
+      }
+
+      // override default path is none set
+      if (!config.path) {
+        const opPath = path.join(
+          viteConfig.root,
+          viteConfig.build.outDir,
+          viteConfig.build.assetsDir,
+        );
+        config.path = opPath;
+      }
+    },
+    transformIndexHtml(html) {
+      const flowPromise = !isProcessed
+        ? favicons.favicons(source, config, callback)
+        : new Promise((resolve) => (resolve(response)));
+      return flowPromise.then((res) => {
+        response = res;
+        isProcessed = true;
+        return html.replace('</head>', `${res.html.join('')}</head>`);
+      }).catch((err) => {
+        throw err;
+      });
+    },
+    closeBundle() {
+      const fileCreationPromise = [];
+      const { files } = response;
+      const { images } = response;
+      files.forEach((eachFile) => {
+        fileCreationPromise.push(writeFile(`${config.path}/${eachFile.name}`, eachFile.contents));
+      });
+      images.forEach((eachImage) => {
+        fileCreationPromise.push(writeFile(`${config.path}/${eachImage.name}`, eachImage.contents));
+      });
+      return Promise.all(fileCreationPromise);
+    },
+  };
+};
+
+export default viteFaviconsPlugin;
